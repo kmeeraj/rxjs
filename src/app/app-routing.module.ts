afc1deaf462b1186d78eb17a8330d38e015c45df
meeraj kanaparthi <meeraj.kanaparthi@teradata.com>
2019-04-15 20:29:05 +0530

    basic app

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import {AboutComponent} from './about/about.component';
+import {HomeComponent} from './home/home.component';
 
-const routes: Routes = [];
+const routes: Routes = [{path: 'about', component: AboutComponent},
+  {path: 'home', component: HomeComponent},
+  { path: '',
+    redirectTo: '/home',
+    pathMatch: 'full'
+  },];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,47 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
+import {
+  MatButtonModule, MatCardModule,
+  MatDatepickerModule, MatIconModule,
+  MatInputModule, MatListModule, MatMenuModule,
+  MatPaginatorModule,
+  MatProgressSpinnerModule,
+  MatSelectModule, MatSidenavModule,
+  MatSortModule, MatTabsModule, MatToolbarModule
+} from '@angular/material';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {HttpClientModule} from '@angular/common/http';
+import {MatTableModule} from '@angular/material/typings/table';
+import {MatDialogModule} from '@angular/material/typings/dialog';
+import {ReactiveFormsModule} from '@angular/forms';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    AboutComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    BrowserAnimationsModule,
+    HttpClientModule,
+    MatMenuModule,
+    MatButtonModule,
+    MatIconModule,
+    MatCardModule,
+    MatTabsModule,
+    MatSidenavModule,
+    MatListModule,
+    MatToolbarModule,
+    MatInputModule,
+    MatPaginatorModule,
+    MatSortModule,
+    MatProgressSpinnerModule,
+    AppRoutingModule,
+
   ],
   providers: [],
   bootstrap: [AppComponent]
