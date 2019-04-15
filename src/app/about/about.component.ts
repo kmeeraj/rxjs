796134ff94900f99c67833c1be90fd5e56ef4469
meeraj kanaparthi <meeraj.kanaparthi@teradata.com>
2019-04-15 21:05:32 +0530

    simple example

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -10,6 +10,18 @@ export class AboutComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
+    document.addEventListener('click', evt => {
+      console.log(evt);
+    });
+    let counter = 0;
+    setInterval(() => {
+      console.log(counter);
+      counter++;
+    },2000);
+
+    setTimeout(() => {
+      console.log('finished..');
+    },4000);
   }
 
 }
