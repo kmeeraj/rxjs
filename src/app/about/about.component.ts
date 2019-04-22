66e0c6c7817d8491641c525f709e3bd5ac3b7c06
meeraj kanaparthi <meeraj.kanaparthi@teradata.com>
2019-04-22 20:11:18 +0530

    basic rxjs observables

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {interval, timer} from 'rxjs';
 
 @Component({
   selector: 'app-about',
@@ -10,6 +11,13 @@ export class AboutComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
+
+    const interval$ = interval(1000);
+    interval$.subscribe(val => console.log('stream 1 ' + val));
+    interval$.subscribe(val => console.log('stream 2 ' + val));
+    const timer$ = timer(3000, 1000);
+    timer$.subscribe(val => console.log('timer 1 ' + val));
+
   }
 
 }
