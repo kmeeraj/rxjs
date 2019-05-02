1ba2a31f21ab5810c0b161051151dc89bc11c9e4
meeraj kanaparthi <meeraj.kanaparthi@teradata.com>
2019-05-02 20:41:34 +0530

    from event

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import {interval, timer} from 'rxjs';
+import {fromEvent, interval, timer} from 'rxjs';
 
 @Component({
   selector: 'app-about',
@@ -14,10 +14,9 @@ export class AboutComponent implements OnInit {
 
     const interval$ = interval(1000);
     interval$.subscribe(val => console.log('stream 1 ' + val));
-    interval$.subscribe(val => console.log('stream 2 ' + val));
-    const timer$ = timer(3000, 1000);
-    timer$.subscribe(val => console.log('timer 1 ' + val));
 
+    const click$ = fromEvent(document, 'click');
+    click$.subscribe(evt => console.log(evt) );
   }
 
 }
