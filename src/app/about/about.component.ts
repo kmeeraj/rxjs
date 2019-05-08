13999e0dbf6097331327973f63b3e337a02ba804
meeraj kanaparthi <meeraj.kanaparthi@teradata.com>
2019-05-08 14:45:15 +0530

    concat

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import {noop, Observable} from 'rxjs';
+import {concat, interval, noop, Observable, of} from 'rxjs';
 import {createObservable} from '../common/utils';
 import {map} from 'rxjs/operators';
 
@@ -13,18 +13,13 @@ export class AboutComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
-    const http$ = createObservable();
-
-    const courses$ = http$
-      .pipe(
-        map(res => Object.values(res['payload']))
-      );
-
-    courses$.subscribe(
-      courses => console.log(courses),
-      noop,
-      () => console.log('completed')
-    );
+    // const source1$ = of(1, 2, 3);
+    const source1$ = interval(1000);
+    const source2$ = of(4, 5, 6);
+    const source3$ = of(7, 8, 9);
+    const result$ = concat(source1$, source2$, source3$);
+    // result$.subscribe(val => console.log(val));
+    result$.subscribe(console.log);
   }
 
 }
