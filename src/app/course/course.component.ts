5b4e63393a6649f9f572ba2ed1d0693a3877d64b
meeraj kanaparthi <meeraj.kanaparthi@teradata.com>
2019-05-17 23:47:24 +0530

    forkjoin and throttle

--- src/app/course/course.component.ts
+++ src/app/course/course.component.ts
@@ -1,10 +1,10 @@
 import {AfterViewInit, Component, ElementRef, OnInit, ViewChild} from '@angular/core';
-import {fromEvent, Observable} from 'rxjs';
+import {forkJoin, fromEvent, Observable} from 'rxjs';
 import {Course} from '../model/course';
 import {Lesson} from '../model/lesson';
 import {ActivatedRoute} from '@angular/router';
 import {createObservable} from '../common/utils';
-import {debounceTime, distinctUntilChanged, map, startWith, switchMap} from 'rxjs/operators';
+import {debounceTime, distinctUntilChanged, map, startWith, switchMap, tap, throttleTime} from 'rxjs/operators';
 import {debug, RxJsLoggingLevel, setRxJsLoggingLevel} from '../common/debug';
 
 @Component({
@@ -24,24 +24,25 @@ export class CourseComponent implements OnInit, AfterViewInit {
 
   ngOnInit() {
     this.courseId = this.route.snapshot.params['id'];
-    this.course$ = createObservable(`/api/courses/${this.courseId}`)
+    const course$ = createObservable(`/api/courses/${this.courseId}`);
+    const lessons$ = this.loadLessons();
+    forkJoin(course$, lessons$)
       .pipe(
-        debug(RxJsLoggingLevel.INFO, 'course')
-      );
-    setRxJsLoggingLevel(RxJsLoggingLevel.TRACE);
+        tap(([course, lessons]) => {
+            console.log('course', course);
+            console.log('lessons', lessons);
+        })
+      )
+      .subscribe();
   }
 
   ngAfterViewInit(): void {
-    this.lessons$ = fromEvent<any>(this.input.nativeElement, 'keyup')
+     fromEvent<any>(this.input.nativeElement, 'keyup')
       .pipe(
         map(event => event.target.value),
-        startWith(''),
-        debug(RxJsLoggingLevel.TRACE, 'search'),
-        debounceTime(400),
-        distinctUntilChanged(),
-        switchMap( search => this.loadLessons(search)),
-        debug(RxJsLoggingLevel.DEBUG, 'lessons value')
-      );
+        throttleTime(500)
+      )
+      .subscribe(console.log);
   }
 
   loadLessons(search = ''): Observable<Lesson[]> {
